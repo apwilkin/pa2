4b5b2e8abe91382ab02b7ccea27016b8e0d27da3
Andrew P Wilkinson <apwilkin@timberlake.cse.buffalo.edu>
2017-04-10 14:14:37 -0400

    GBN passes all tests now

--- src/gbn.cpp
+++ src/gbn.cpp
@@ -28,7 +28,7 @@
 bool timeout = false;
 bool wrongack = false;
 bool nextmsg = false;
-bool resettimer = false;
+bool reset = false;
 int seqnum = -1;
 int acknum = -1;
 int a_seqnum = 0;
@@ -95,9 +95,18 @@ uint16_t gen_crc16(const uint8_t *data, uint16_t size) {
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message) {
+	/*
+	outputfile.open ("output_data.txt", std::ios_base::app);
+	outputfile << "A output called\n";
+	outputfile << "Input data: ";
+	outputfile << *(message.data);
+	outputfile << "\n";
+	outputfile.close();
+	*/
+	
 
 	// if this is a new message then increment seq and ack nums
-	if (!(timeout || wrongack || nextmsg)) {
+	if (!(timeout)) {
 			seqnum++;
 			acknum++;
 	}
@@ -113,104 +122,212 @@ void A_output(struct msg message) {
 	packet.checksum = sum;
 	
 	// if timeout or wrongack send entire inflight_queue
-	if (timeout || wrongack) {
+	if (timeout) {
+		/*
 		outputfile.open ("output_data.txt", std::ios_base::app);
-		outputfile << "Timeout or Wrongack\n";
-		outputfile.close();
+		outputfile << "Timeout\n";
+		*/
 		timeout = false;
-		wrongack = false;
+		//wrongack = false;
+		
+		//refill queue
 		while ((inflight_queue.size() < winsize) && !(pkt_queue.empty())) {
 			pkt temp = pkt_queue.front();
 			pkt_queue.pop();
 			inflight_queue.push_back(temp);
 		}
-		for (int i = 0; i < inflight_queue.size(); i++) {
-			tolayer3(0, inflight_queue[i]);
-			starttimer(0, 20);
+		
+		//if queue's not empty restart timer
+		if (!(inflight_queue.empty())) {
+			/*
+			outputfile << "Resend timer restart \n";
+			*/
+			starttimer(0, 15);
 		}
+		
+		//resend all packets inflight queue
+		for (int i = 0; i < inflight_queue.size(); i++) {
+			/*
+			outputfile << "Resend (timeout) message sent from A: ";
+			outputfile << *inflight_queue[i].payload;
+			outputfile << "\n";
+			*/
+			tolayer3(0, inflight_queue[i]);
+		}
+		/*
+		outputfile.close();
+		*/
 	}
 	
 	// else if nextmsg, if inflight < windowsize pop from pkt_queue, add to inflight_queue and send
-	else if (nextmsg) {
+	else if (true == false) {
+		/*
 		outputfile.open ("output_data.txt", std::ios_base::app);
 		outputfile << "Next message\n";
 		outputfile.close();
+		*/
 		nextmsg = false;
-		if ((inflight_queue.size() < winsize) && !(pkt_queue.empty())) {
+		
+		// if there are messages in flight or messages to be put in flight restart timer
+		if (!(inflight_queue.empty()) || !(pkt_queue.empty())) {
+			/*
+			outputfile.open ("output_data.txt", std::ios_base::app);
+			outputfile << "Messages in flight; restart timer\n";
+			outputfile.close();
+			*/
+			starttimer(0, 15);
+		}
+		
+		// refill inflight queue (send each new message)
+		while ((inflight_queue.size() < winsize) && !(pkt_queue.empty())) {
+			/*
 			outputfile.open ("output_data.txt", std::ios_base::app);
 			outputfile << "Next message refill loop\n";
-			outputfile.close();
+			*/
 			pkt temp = pkt_queue.front();
 			pkt_queue.pop();
-			inflight_queue.push_back(temp);
 			tolayer3(0, temp);
-			starttimer(0, 20);
+			inflight_queue.push_back(temp);
+			
+			/*
+			outputfile << "Next message send from A: ";
+			outputfile << *temp.payload;
+			outputfile << "\n";			
+			outputfile.close();
+			*/
 		}
 	}
+	
 	// else if new packet
 	else {
 		//  if inflight_queue < windowsize add to inflight_queue and send
-		/**/
+		/*
 		outputfile.open ("output_data.txt", std::ios_base::app);
-		outputfile << "New packet from A \n";
+		outputfile << "New packet from A: ";
 		outputfile << *packet.payload;
 		outputfile << "\n";
-		/**/
+		*/
 		if (inflight_queue.size() < winsize) {
-			//reset timer if empty
-			if (inflight_queue.empty()) {
-				starttimer(0, 20);
+			// if inflight queue isn't full double check to see if there aren't queued packets
+			while ((inflight_queue.size() < winsize) && !(pkt_queue.empty())) {
+				pkt temp = pkt_queue.front();
+				pkt_queue.pop();
+				tolayer3(0, temp);
+				inflight_queue.push_back(temp);
 			}
-			inflight_queue.push_back(packet);
-			tolayer3(0, packet);
+			// if it's empty timer needs to be started
+			if (inflight_queue.empty()) {
+				starttimer(0, 15);
+				inflight_queue.push_back(packet);
+				tolayer3(0, packet);
+			}
+			// if it's still not full add to queue and send;
+			else {
+				inflight_queue.push_back(packet);
+				tolayer3(0, packet);
+			}			
+			/*
+			outputfile << "New send from A: ";
+			outputfile << *packet.payload;
+			outputfile << "\n";
+			*/
 		}
 		//  else add to pkt_queue
 		else {
-
 			pkt_queue.push(packet);
 		}
-		/**/
+		/*
 		outputfile.close();
-		/**/
+		*/
 	}
 	
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet) {
-	//right or wrong ack?
+	//right ack
 	if ((packet.acknum == a_acknum) || (packet.seqnum == a_seqnum)) {
+		/*
 		outputfile.open ("output_data.txt", std::ios_base::app);
 		outputfile << "Successful transmission!\n";
-		outputfile.close();
+		outputfile << "(Final Check)\n";
+		outputfile << "packet.acknum: ";
+		outputfile << packet.acknum;
+		outputfile << "\n";
+		outputfile << "a_acknum: ";
+		outputfile << a_acknum;
+		outputfile << "\n";
+		outputfile << "packet.seqnum: ";
+		outputfile << packet.seqnum;
+		outputfile << "\n";
+		outputfile << "a_seqnum: ";
+		outputfile << a_seqnum;
+		outputfile << "\n";
+		
+		*/
+		
+		outputfile << "packet being popped: ";
+		outputfile << *((inflight_queue.front()).payload);
+		outputfile << "\n";
 		inflight_queue.pop_front();
+		
 		a_acknum++;
 		a_seqnum++;
 		nextmsg = true;
+		stoptimer(0);
+		outputfile.close();
+		
+		//maybe add to inflight and send from here? then reset timer (without calling a agian) and restart timer
+		
+		if ((!inflight_queue.empty() && inflight_queue.size() < winsize) || !pkt_queue.empty()) {
+			starttimer(0, 15);
+			while ((inflight_queue.size() < winsize) && !(pkt_queue.empty())) {
+				/*
+				outputfile.open ("output_data.txt", std::ios_base::app);
+				outputfile << "Next message refill loop\n";
+				*/
+				pkt temp = pkt_queue.front();
+				pkt_queue.pop();
+				tolayer3(0, temp);
+				inflight_queue.push_back(temp);
+			
+				/*
+				outputfile << "Next message send from A: ";
+				outputfile << *temp.payload;
+				outputfile << "\n";			
+				outputfile.close();
+				*/
+			}	
+		}
+		else {
+			return;
+		}
 	}
 	else {
 		stoptimer(0);
-		wrongack = true;
 	}
-	msg temp;
-	A_output(temp);
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt() {
-	timeout = true;
-	msg temp;
-	A_output(temp);
+	if (nextmsg) {
+		nextmsg = false;
+	}
+	else {
+		timeout = true;
+		msg temp;
+		A_output(temp);
+	}
 }  
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init() {
-	/**/
+	/*
 	outputfile.open ("output_data.txt");
 	outputfile << "~~~~~~~~~~~~~~~~~ A_init called ~~~~~~~~~~~~~~~~~~~~~~\n";
 	outputfile.close();
-	/**/
+	*/
 	winsize = getwinsize();
 }
 
@@ -219,9 +336,9 @@ void A_init() {
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet) {
 
-	/**/
+	/*
 	outputfile.open ("output_data.txt", std::ios_base::app);
-	outputfile << "Data received by B \n";
+	outputfile << "Data received by B: ";
 	outputfile << *packet.payload;
 	outputfile << "\n";
 	outputfile << "Actual Seqnum: ";
@@ -237,7 +354,7 @@ void B_input(struct pkt packet) {
 	outputfile << b_acknum;
 	outputfile << "\n";
 	outputfile.close();
-	/**/
+	*/
 
 	uint16_t a_checksum = packet.checksum;
 	uint16_t reset = 0;
@@ -252,11 +369,11 @@ void B_input(struct pkt packet) {
 	// else if next expected
 	else if (packet.seqnum == b_seqnum && packet.acknum == b_acknum) {
 		//this is the one we expected
-		/**/
+		/*
 		outputfile.open ("output_data.txt", std::ios_base::app);
 		outputfile << "(This is the next expected from B)\n";
 		outputfile.close();
-		/**/
+		*/
 		b_seqnum++;
 		b_acknum++;
 		tolayer5(1, packet.payload);
@@ -276,3 +393,4 @@ void B_input(struct pkt packet) {
 void B_init() {
 	winsize = getwinsize();
 }
+
